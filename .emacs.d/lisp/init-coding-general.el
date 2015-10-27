7e4427d806b73cb382025616cd4430b4eca29d56
Wai <johnchunwai@gmail.com>
2015-10-26 22:01:32 -0700

    disable smartparens for c modes; highlight lines longer than 80 column for program modes

--- .emacs.d/lisp/init-coding-general.el
+++ .emacs.d/lisp/init-coding-general.el
@@ -8,8 +8,13 @@
 
 (show-paren-mode 1)
 
-(global-subword-mode t)
+(global-subword-mode 0)
 
+(setq-default
+ whitespace-line-column 80
+ whitespace-style '(face lines-tail))
+
+(add-hook 'prog-mode-hook #'whitespace-mode)
 
 (provide 'init-coding-general)
 
